a9153df0ec897777e1d5a26388ccafc7a51e3f9f
Qmun14 <mamunramdhan@gmail.com>
2023-05-12 20:48:16 +0700

    Protect API with JWT Access Token, memproteksi RESTfull API dengan JWT Access Token

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,9 +1,20 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+import { verifyJwt } from '@/lib/jwt';
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request, { params }: { params: { id: number } }) {
+  const accessToken = request.headers.get("authorization");
+  if (!accessToken || !verifyJwt(accessToken)) {
+    return new Response(JSON.stringify({
+      error: "unauthorized",
+    }),
+      {
+        status: 401,
+      },
+    );
+  }
   const userPosts = await prisma.post.findMany({
     where: { authorId: +params.id },
     include: {
